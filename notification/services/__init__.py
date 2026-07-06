7d0ad22418de987737d26ff16301113727f20b3d
arpit-tesseract <arpittt053@tesseracttechnolabs.com>
2026-07-06 15:31:32 +0530

    Delete notification directory

--- notification/services/__init__.py
+++
@@ -1,7 +0,0 @@
-# notification/services/__init__.py
-# Exposes the public service-layer API so callers only need to import from
-# `notification.services` rather than digging into sub-modules.
-
-from .dispatcher import trigger_event  # noqa: F401
-
-__all__ = ["trigger_event"]
